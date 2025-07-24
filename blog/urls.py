099c39df14bd7b1074255642e500867c7e4482f9
Pezz-x <145380526+Pezz-x@users.noreply.github.com>
2025-07-24 16:06:26 +0100

    fix urls and views

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,4 @@ from . import views # import views.py from the current directory
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('', include("app_name_urls"),
-    name="app_name_urls"), # the app urls are loaded asthe main urls
     ]
