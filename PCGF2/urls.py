73395d23ee0ab5e5da7308d4d71c07ae264f73aa
Pezz-x <145380526+Pezz-x@users.noreply.github.com>
2025-07-25 10:33:06 +0100

    fix path is PCGF2/urls.py file

--- PCGF2/urls.py
+++ PCGF2/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),  # Connect app URLs to main project
+    path('', include('blog.urls')),  # Connect app URLs to main project
 ]
