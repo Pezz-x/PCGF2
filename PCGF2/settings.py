cbebbf5a9d2cf824215a4800fc549eabfc54495e
Pezz-x <145380526+Pezz-x@users.noreply.github.com>
2025-07-25 10:44:02 +0100

    fix herokuapp in allowed hosts

--- PCGF2/settings.py
+++ PCGF2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%8z@7&%09uzayyesjnw!lhl&rj#fja!*&xf6=5a@(l9+3^+zqg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp','127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
 
 # Application definition
