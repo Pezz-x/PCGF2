5ef2408e7a849a2d72b62f1d56614bb95c0677ba
Pezz-x <145380526+Pezz-x@users.noreply.github.com>
2025-07-29 14:50:19 +0100

    Add .gitignore, update settings for environment variables, and add blog models and migrations blog posts can now be made and viewed in admin pannel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Post
+#from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
+@admin.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+
+    list_display = ('author', 'title', 'slug', 'time_created')
+    search_fields = ['author', 'title', 'body']
+    list_filter = ('author', 'time_created')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('body',)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    body = models.TextField()
+    # images = TODO
+    time_created = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-time_created"]
+
+    def __self__(self):
+        return f"{self.title}| wiritten by {self.author}"
