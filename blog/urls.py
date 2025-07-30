ed9b27cd04d270f4d6f0b36d999451e1d5f9c28d
Pezz-x <145380526+Pezz-x@users.noreply.github.com>
2025-07-30 19:39:23 +0100

    delete blog app

--- blog/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path # import path, similar toproject's urls.py
-from . import views # import views.py from the current directory
-
-urlpatterns = [
-    path('', views.home, name='home'),
-]
--- blog/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, get_object_or_404, reverse
-from django.views import generic
-from django.contrib import messages
-from django.http import HttpResponseRedirect
-from .models import Post, Comment
-
-# Create your views here.
-def home(request):
-    return render(request, 'index.html')
