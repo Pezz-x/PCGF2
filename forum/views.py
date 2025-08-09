eaa4e3fcbbc938c136a72bebad13b985974ffe2b
Pezz-x <145380526+Pezz-x@users.noreply.github.com>
2025-08-09 11:44:47 +0100

    added add_comment to urls and views

--- forum/views.py
+++ forum/views.py
@@ -26,32 +26,15 @@ def post_detail(request, slug):
 
 # POST DETAIL PAGE
 
+
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
     comments = post.comments.all()
     comment_form = CommentForm()
-    user_liked_post = request.user.is_authenticated and request.user in post.liked_by.all()
-
-    if request.method == 'POST' and request.user.is_authenticated:
-        comment_form = CommentForm(request.POST, request.FILES)
-        if comment_form.is_valid():
-            comment = comment_form.save(commit=False)
-            comment.post = post
-            comment.author = request.user
-            comment.save()
-            return redirect('post_detail', slug=post.slug)
-
-    comment_likes = {}
-    if request.user.is_authenticated:
-        for c in comments:
-            comment_likes[c.id] = (request.user in c.liked_by.all())
-
-    return render(request, 'forum/post_detail.html', {
+    return render(request, 'post_detail.html', {
         'post': post,
         'comments': comments,
-        'comment_form': comment_form,
-        'user_liked_post': user_liked_post,
-        'comment_likes': comment_likes,
+        'comment_form': comment_form
     })
 
 
@@ -93,6 +76,22 @@ def post_like_toggle(request, slug):
         post.liked_by.add(user)
         liked = True
     return JsonResponse({'liked': liked, 'likes_count': post.likes_count})
+
+@login_required
+@require_POST
+def add_comment(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect('post_detail', slug=slug)
+    else:
+        form = CommentForm()
+    return render(request, 'post_detail.html', {'post': post, 'comment_form': form})
 
 
 @login_required
